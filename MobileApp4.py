0829faff171f896aeff79a5922ea2c84a41d76b6
oliversGit-source <obutterworthbakhshi@icloud.com>
2025-02-19 22:45:27 +0000

    Update MobileApp4.py

--- MobileApp4.py
+++ MobileApp4.py
@@ -8,7 +8,7 @@ st.image('https://www.lendingtree.com/content/uploads/2020/01/mortgage-default-1
 
 
 # Textbox for user input
-number = st.number_input("How good is your predictive model? Enter the decimal percentage of false positives: (formula = (1 - specificity) Hint. Type in 0.999, that's the one from my best ML model ;)):", min_value=0)
+number = st.number_input("How good is your predictive model? Enter the percentage of FP (false positives): (formula = (1 - specificity)", min_value=0)
 
 error_rate = 1 - number
 baseline = 0.05
@@ -16,11 +16,6 @@ baseline = 0.05
 cost = (int(0.35*200442*279606*error_rate)) / 1000000
 cost_baseline = (int(0.35*200442*279606*0.05)) / 1000000
 savings = (cost_baseline - cost)
-
-#create a dictionary
-models2 = {'Cost with predictive models' : cost, 
-           'Cost without models': cost_baseline, 
-           'Total annual saving': savings}
 
 # Show the result
 st.write(f'Cost with predictive models: {cost :.3f} million', f'Cost without models: {cost_baseline :.3f} million', f'The total saving to the UK Banking industry from incorrect Loan Classification is: {savings :.3f} million.')
