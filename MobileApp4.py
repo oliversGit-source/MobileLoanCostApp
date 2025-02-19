7836f8baa7f7e9d3ad1660977cdde502c38aa26e
oliversGit-source <obutterworthbakhshi@icloud.com>
2025-02-19 22:38:15 +0000

    Update MobileApp4.py

--- MobileApp4.py
+++ MobileApp4.py
@@ -13,8 +13,8 @@ number = st.number_input("How good is your predictive model? Enter the decimal p
 error_rate = 1 - number
 baseline = 0.05
 # Calculation (example: square the number)
-cost = (int(0.35*200442*279606*error_rate)) / 1000000
-cost_baseline = (int(0.35*200442*279606*0.05)) / 1000000
+cost = (int(0.35*200442*279606*error_rate)) / 1000
+cost_baseline = (int(0.35*200442*279606*0.05)) / 1000
 savings = (cost - cost_baseline)
 
 #create a dictionary
