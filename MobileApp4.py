f2f8738a06235648139b6ca383acb713dfd167f5
oliversGit-source <obutterworthbakhshi@icloud.com>
2025-02-20 14:02:59 +0000

    Update MobileApp4.py

--- MobileApp4.py
+++ MobileApp4.py
@@ -6,10 +6,10 @@ st.title('Loan Classification Predictive Model: \n How much do you think incorre
 
 st.image('https://www.lendingtree.com/content/uploads/2020/01/mortgage-default-1140x524.jpg')
 
-st.text("Precision Specificity measures the proportion of actual negative instances that were correctly identified by the classifier. It complements recall by focusing on the negative class, providing insight into the classifier’s ability to avoid false positives.")
+st.text("Precision Specificity measures the proportion of actual negative instances that were correctly identified by the classifier. It provides insight into the classifier’s ability to avoid false positives.")
 
 # Textbox for user input
-number = st.number_input("How good is your predictive model? Enter the specificity of your model:", min_value=0)
+number = st.number_input("How good is your predictive model? Enter the specificity of your model below, and then click the 'Calculate' button.", min_value=0)
 
 
 if(st.button('Calculate')):
@@ -18,16 +18,18 @@ if(st.button('Calculate')):
            error_rate = 1 - number
            baseline = 0.05
            # Calculation (example: square the number)
-           cost = int(0.35*200442*279606*error_rate)/1000000
-           cost_baseline = int(0.35*200442*279606*0.05)/1000000
-           savings = (cost_baseline - cost)
+           cost = int(0.35*200442*279606*error_rate)
+           cost_in_mill = cost/1000000
+           cost_baseline = int(0.35*200442*279606*0.05)
+           cost_baseline_in_mill = cost_baseline/1000000
+           savings = (cost_baseline_in_mill - cost_in_mill)
 
 
-           models2 = {'Cost with predictive models' : cost, 
-                      'Cost without models': cost_baseline, 
+           models2 = {'Cost with predictive models' : cost_in_mill, 
+                      'Cost without models': cost_baseline_in_mill, 
                           'Total annual saving': savings}
            
-           st.write(f'Cost with predictive models: {cost :.3f} million', f'Cost without models: {cost_baseline :.3f} million', f'The total saving to the UK Banking industry from incorrect Loan Classification is: {savings :.3f} million.')
+           st.write(f'Cost with predictive models: {cost_in_mill :.3f} million', f'Cost without models: {cost_baseline_in_mill :.3f} million', f'The total saving to the UK Banking industry from incorrect Loan Classification is: {savings :.3f} million.')
 
 # Create a button, that when clicked, shows a text
 if(st.button("About")):
