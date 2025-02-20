73e3c10386751a251c444be5a815aef528bf85b7
oliversGit-source <obutterworthbakhshi@icloud.com>
2025-02-20 14:35:34 +0000

    Update MobileApp4.py

--- MobileApp4.py
+++ MobileApp4.py
@@ -9,7 +9,7 @@ st.image('https://www.lendingtree.com/content/uploads/2020/01/mortgage-default-1
 st.write("Precision Specificity measures the proportion of actual negative instances that were correctly identified by the classifier.  \nIt provides insight into the classifier’s ability to avoid false positives.  \nThe best performing predictive model in this study had a specificity of 0.999.")
 
 # Textbox for user input
-number = st.number_input("How good is your predictive model? Enter the specificity of your model below, and then click the 'Calculate' button.")
+number = st.number_input("How good is your predictive model? Enter the specificity of your model below, and then click the 'Calculate' button.", placeholder="Type a number here...")
 
 st.write("The specificity of your predicted model is:", number)
 
@@ -28,7 +28,7 @@ models2 = {'Cost with predictive models' : cost_in_mill,
 
 if(st.button('Calculate')):
 # Show the result
-           st.write(f'Cost with predictive models: {cost_in_mill :.3f} million', f'Cost without models: {cost_baseline_in_mill :.3f} million', f'The total saving to the UK Banking industry from incorrect Loan Classification using this predictive ML model is: {savings :.3f} million.')
+           st.write(f'Cost with predictive model: {cost_in_mill :.3f} million', f'Cost without predictive models: {cost_baseline_in_mill :.3f} million', f'The total saving to the UK Banking industry from incorrect Loan Classification using this predictive ML model is: {savings :.3f} million.')
 
 # Create a button, that when clicked, shows a text
 if(st.button("About")):
