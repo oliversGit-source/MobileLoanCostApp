ece921f30205d853b1bdf6c46675aa1b8fe56729
oliversGit-source <obutterworthbakhshi@icloud.com>
2025-02-20 14:10:23 +0000

    Update MobileApp4.py

--- MobileApp4.py
+++ MobileApp4.py
@@ -10,24 +10,22 @@ st.text("Precision Specificity measures the proportion of actual negative instan
 
 # Textbox for user input
 number = st.number_input("How good is your predictive model? Enter the specificity of your model below, and then click the 'Calculate' button.", min_value=0)
+ 
+error_rate = 1 - number
+baseline = 0.05
+# Calculation (example: square the number)
+factor = (0.35*200442*279606)/1000000
+cost_in_mill = factor*error_rate
+cost_baseline_in_mill = factor*baseline
+savings = (cost_baseline_in_mill - cost_in_mill)
 
+
+models2 = {'Cost with predictive models' : cost_in_mill, 
+           'Cost without models': cost_baseline_in_mill, 
+           'Total annual saving': savings}
 
 if(st.button('Calculate')):
 # Show the result
-           
-           error_rate = 1 - number
-           baseline = 0.05
-           # Calculation (example: square the number)
-           factor = (0.35*200442*279606)/1000000
-           cost_in_mill = factor*error_rate
-           cost_baseline_in_mill = factor*baseline
-           savings = (cost_baseline_in_mill - cost_in_mill)
-
-
-           models2 = {'Cost with predictive models' : cost_in_mill, 
-                      'Cost without models': cost_baseline_in_mill, 
-                          'Total annual saving': savings}
-           
            st.write(f'Cost with predictive models: {cost_in_mill :.3f} million', f'Cost without models: {cost_baseline_in_mill :.3f} million', f'The total saving to the UK Banking industry from incorrect Loan Classification is: {savings :.3f} million.')
 
 # Create a button, that when clicked, shows a text
