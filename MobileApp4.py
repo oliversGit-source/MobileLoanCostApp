345b4a31ea7b5aadf6d25f7449415355ced5144c
oliversGit-source <obutterworthbakhshi@icloud.com>
2025-02-20 14:51:50 +0000

    Update MobileApp4.py

--- MobileApp4.py
+++ MobileApp4.py
@@ -35,9 +35,8 @@ if(st.button("About")):
     st.text("This is an online mobile app created by Oliver Butterworth-Bakhshi for an Imperial Business School Data Analytics Project.")
 
 
-# Collect user feedback
-feedback = streamlit_feedback(feedback_type="thumbs", optional_text_label="[Optional] Please provide an explanation", on_submit=on_submit)
-                
-# Get user feedback and store it
-if feedback is not None:
-           on_submit(feedback)
+sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+selected = st.feedback("thumbs" optional_text_label="[Optional] Please provide feedback:", on_submit=on_submit)
+if selected is not None:
+    st.markdown(f"You selected: {sentiment_mapping[selected]}")
+    on_submit(selected)
